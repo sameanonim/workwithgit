f15c8886ba98451c781f8afb9a8051e1dc1f352b
samea <sameanoniM@gmail.com>
2023-02-02 12:54:58 +0300

    new feature

--- str_func.py
+++ str_func.py
@@ -1,3 +1,13 @@
 def caplett(word):
+    """
+    делает буквы заглавными
+    """
     s = input(word)
     return s.upper()
+
+def partcaplett(word):
+    """
+    делает заглавными первые буквы каждого слова
+    """
+    s = input(word)
+    return s.title()
